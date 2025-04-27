bd2db7e5a49a69a58bad2af8b36fedfb67eb7a83
William Morillo <morillow02@gmail.com>
2025-04-26 21:18:34 -0400

    password

--- src/confing/db.js
+++ src/confing/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = mySql2.createPool({
   host: process.env.MYSQLHOST, // Usar la variable 'MYSQLHOST' proporcionada por Railway
   user: process.env.MYSQLUSER, // Usar la variable 'MYSQLUSER' proporcionada por Railway
-  password: process.env.MYSQLPASSWORD, // Usar la variable 'MYSQL_ROOT_PASSWORD' proporcionada por Railway
+  password: process.env.MYSQL_ROOT_PASSWORD, // Usar la variable 'MYSQL_ROOT_PASSWORD' proporcionada por Railway
   database: process.env.MYSQL_DATABASE, // Usar la variable 'MYSQL_DATABASE' proporcionada por Railway
   port: process.env.MYSQLPORT,
   waitForConnections: true,
